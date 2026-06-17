a01ae83ceab9278c9c7ff0c6e68bda0f42873765
2022hakone-oss <2022hakone@gmail.com>
2026-06-17 18:04:13 +0900

    Gemfile.lockの追加とコントローラーの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,17 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    # ネストされたURLからpost_idを取得し、対象の投稿を特定
-    @post = Post.find(params[:post_id])
-    # 特定した投稿に紐づくコメントを作成
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.build(comment_params)
+  @comment.user_id = current_user.id # ログインユーザーを紐付ける場合
+
+  if @comment.save
+    redirect_to @post, noble: "コメントを投稿しました！"
+  else
+    # 💡 保存に失敗した場合、エラー内容を画面に警告として出す
+    redirect_to @post, alert: "コメントの保存に失敗しました: #{@comment.errors.full_messages.join(', ')}"
   end
+end
 
   private
 
